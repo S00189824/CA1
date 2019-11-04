25c50cb48e4c7504cb111fa262d08a3d9bdfb1c0
David Coleman <46888854+S00189824@users.noreply.github.com>
2019-11-04 12:35:14 +0000

    CA1 Finished

---
+++ CA1/CA1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Can't figure out how to get enums to work need to work on this more.
+            Song S1 = new Song("Avolon", "Serinity", 4.32);
+            Song S2 = new Song("Everlast", "22 Step", 2.45);
+            Song S3 = new Song("America", "Eminem", 3.06);
+            Song S4 = new Song("123 fake track", "Leona", 4);
+            Song S5 = new Song("To the end", "Excella", 3);
+
+            List<Song> Playlist = new List<Song>();
+            Playlist.Add(S1);
+            Playlist.Add(S2);
+            Playlist.Add(S3);
+            Playlist.Add(S4);
+            Playlist.Add(S5);
+            
+
+            Display(Playlist);
+            Console.WriteLine();
+            Console.WriteLine("Sorting the Artist by Artist,then song title");
+            Playlist.Sort();
+            Display(Playlist);
+            Console.WriteLine();
+            Console.WriteLine("Reshuffleing the playlist");
+            Reshuffle(Playlist);
+            Display(Playlist);
+        }
+        public static void Reshuffle(List<Song>Songshuffle)
+        {
+            int randomsong;
+            Random shuffle = new Random();//reshuffle method here
+            randomsong = shuffle.Next(1,5);//tried the random but not sure here
+            //Songshuffle = shuffle
+        }
+
+        public static void Display(List<Song>playlist)
+        {
+            foreach (Song song in playlist)
+            {
+                Console.WriteLine("{0,-10}{1,-10}{2,-10}",song._Artist,song._title,song._Duration,Genre.Dance);
+            }
+        }
+    }
+}
---
+++ CA1/CA1/Song.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA1
+{
+    public enum Genre
+    {
+        Rock,
+        Pop,
+        Dance,
+        Other
+    }
+    class Song : IComparable
+    {
+       
+
+        public string _title { get; set; }
+        public string _Artist { get; set; }
+        public double _Duration { get; set; }
+        public Genre _genre { get; set; }
+
+        public Song(string title,string artist,Genre genre)
+        {
+            _title = title;
+            _Artist = artist;
+            this._genre = genre;
+        }
+
+        public void AddGenre(Genre CurrentGenre)
+        {
+            
+        }
+
+        public Song(string name, string artist, double duration)
+            : this(name, artist, Genre.Other)
+        {
+            _title = name;
+            _Artist = artist;
+            _Duration = duration;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Artist {0}  Song {1} Duration {2} Genre {3}", _Artist, _title, _Duration,_genre);
+        }
+
+        public int CompareTo(object obj)// I comparable
+        {
+            Song ObjectthatIAmComparingTo = (Song)obj;
+            return this._Artist.CompareTo(ObjectthatIAmComparingTo._Artist);
+        }
+    }
+}
